8e1ad054ac8abb53cba54fb3ba6bb2df9c5aaa3a
rongrongrr <jingrong.contact@gmail.com>
2019-11-17 15:28:43 +0800

    Implement FoodGeneratorBot

---
+++ src/main/java/FoodGeneratorBot.java
@@ -0,0 +1,32 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class FoodGeneratorBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "makan_where_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "987153015:AAG1JhqI8rx0oxAGXWXZgBr54qyDNl_HnaY";
+    }
+}
